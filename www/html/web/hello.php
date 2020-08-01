5155d93faf08658bf1104440ff21ec2566a84393
hiromu.chiba <fuyu.fuyu447@outlook.jp>
2020-08-01 11:49:47 +0900

    fix

--- www/html/web/hello.php
+++ www/html/web/hello.php
@@ -7,7 +7,6 @@ try {
     print '接続に成功しました。' . '<br>';
     $sql = 'SELECT * FROM test ORDER BY id';
     $lists = ($db->query($sql)->fetchALL(PDO::FETCH_ASSOC));
-
     foreach ($lists as $list) {
         print $list['id'] . '<br>';
         print $list['test_name'] . '<br>';
