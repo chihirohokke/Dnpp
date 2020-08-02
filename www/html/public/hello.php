ceaac4452e3d3b60ed7385d237961b3877a9a9dc
hiromu.chiba <fuyu.fuyu447@outlook.jp>
2020-08-02 10:45:29 +0900

    edit docker , test composer

--- www/html/public/hello.php
+++ www/html/public/hello.php
@@ -1,13 +1,14 @@
 <?php
 
-require_once '../app/DbManager.php';
+require_once '../private/DbManager.php';
 
 try {
     $db = getDb();
+    print 'Hello, PHP!!' . '<br>';
     print '接続に成功しました。' . '<br>';
-    $sql = 'SELECT * FROM test ORDER BY id';
+    print '<hr>';
+    $sql = 'SELECT * FROM tests ORDER BY id';
     $lists = ($db->query($sql)->fetchALL(PDO::FETCH_ASSOC));
-
     foreach ($lists as $list) {
         print $list['id'] . '<br>';
         print $list['test_name'] . '<br>';
